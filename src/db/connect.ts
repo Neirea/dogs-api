4deb028597af85fefb5a0e2dcacc2835e6163511
neirea <neireasoultix@gmail.com>
2023-06-26 02:15:26 +0300

    simplified db connection

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,24 +1,13 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-class Database {
-    private static instance: Sequelize | undefined;
-    static getInstance() {
-        if (Database.instance) {
-            return Database.instance;
-        }
-        Database.instance = new Sequelize({
-            dialect: "mssql",
-            database: process.env.DB_NAME,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            host: process.env.DB_HOST,
-            port: +process.env.DB_NAME!,
-        });
-        return Database.instance;
-    }
-}
-
-const db = Database.getInstance();
+const db = new Sequelize({
+    dialect: "mssql",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_NAME!,
+});
 
 export default db;
